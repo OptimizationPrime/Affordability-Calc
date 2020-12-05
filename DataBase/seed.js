c7886cc39301e993cfc99a2794c8dff9bd30276b
shortdaddy0711 <sodoatm@gmail.com>
2020-12-04 21:19:14 -0800

    back to avocado schema

--- DataBase/seed.js
+++ DataBase/seed.js
@@ -21,6 +21,39 @@ const createProperty = () => {
   return `${userId},${title},${content},${image},${date}\n`
 }
 
+const startWriting = (writeStream, encoding, done) => {
+  let i = lines
+  function writing(){
+    let canWrite = true
+    do {
+      i--
+      let post = createPost()
+      //check if i === 0 so we would write and call `done`
+      if(i === 0){
+        // we are done so fire callback
+        writeStream.write(post, encoding, done)
+      }else{
+        // we are not done so don't fire callback
+        writeStream.write(post, encoding)
+      }
+      //else call write and continue looping
+    } while(i > 0 && canWrite)
+    if(i > 0 && !canWrite){
+      //our buffer for stream filled and need to wait for drain
+      // Write some more once it drains.
+      writeStream.once('drain', writing);
+    }
+  }
+  writing()
+}
+
+//write our `header` line before we invoke the loop
+stream.write(`userId,title,content,image,date\n`, 'utf-8')
+//invoke startWriting and pass callback
+startWriting(stream, 'utf-8', () => {
+  stream.end()
+})
+
 CREATE TABLE affordability.Property (
   propertyId CHAR(10)    PRIMARY KEY,
   address1   VARCHAR(30) NOT NULL,
